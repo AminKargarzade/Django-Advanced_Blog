4bf9af3716b0b3fb9af281924a7b134175c7252d
Amin <aminkargarzadeh26@gmail.com>
2026-02-22 19:55:24 +0330

    Creating Blog models!
    Created post and category!

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """This is a class to define posts for the blog app"""
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+        
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    """This is a class to define categories for the blog app"""
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
