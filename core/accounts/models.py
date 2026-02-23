cf481ef3766ba5e60efd4d29e4b616fa50169dec
Amin <aminkargarzadeh26@gmail.com>
2026-02-23 18:22:58 +0330

    Initial Settings for Custom Usermodel!
    FIrst steps of a setting configuration for applying our own Custom Usermodel in settings.py with AUTH_USER_MODEL =  "accounts.User"!

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -38,7 +38,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verrified = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=255)
+    
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
     created_date = models.DateTimeField(auto_now_add=True)
--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "blog",
+    # "blog",
     
 ]
 
@@ -124,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#UserManager Config
+AUTH_USER_MODEL = 'accounts.User'
