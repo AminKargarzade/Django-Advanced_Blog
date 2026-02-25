2cfa104f55189533f42d9e0438ea28e3511023a2
Amin <aminkargarzadeh26@gmail.com>
2026-02-25 11:29:43 +0330

    Profile Model
    Created Profile Model and Registered it in admin.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,5 @@
+from pydoc import describe
+
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin
@@ -48,3 +50,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    """
+    Profile model that extends the User model with additional fields.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=250, blank=True)
+    last_name = models.CharField(max_length=250, blank=True)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
