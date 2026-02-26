10bed942a8a770a8ac59be4ddc84c42a6a5812f8
Amin <aminkargarzadeh26@gmail.com>
2026-02-26 19:33:07 +0330

    RedirectView
    Start point of RedirectView in CBV part!

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+
+app_name = "blog"
 
 urlpatterns = [
     path('fbv-index', views.indexView, name='fbv-index'),
     # path('cbv-index', TemplateView.as_view(template_name="index.html", extra_context={'name':'amin'})),
     path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
+    path('go-to-index/', RedirectView.as_view(pattern_name="blog:cbv-index"), name='redirect-to-index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -4,11 +4,17 @@ from .models import Post
 
 # Create your views here.
 def indexView(request):
+    """
+    This is a function-based view that renders the 'index.html' template with a context containing a name.
+    """
     name = 'Amin'
     context = {'name': name}
     return render(request, 'index.html', context)
 
 class IndexView(TemplateView):
+    """
+    This is a class-based view that renders the 'index.html' template with a context containing a name and a list of posts.
+    """
     template_name = 'index.html'
     
     def get_context_data(self, **kwargs):
