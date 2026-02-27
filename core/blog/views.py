d43dd3e722f1532a289293db1ea0b391f0571ba0
Amin <aminkargarzadeh26@gmail.com>
2026-02-27 15:37:00 +0330

    RedirectView CBV

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+# Function-based view to Show a template with a context
+'''
 def indexView(request):
     """
     This is a function-based view that renders the 'index.html' template with a context containing a name.
@@ -10,7 +13,7 @@ def indexView(request):
     name = 'Amin'
     context = {'name': name}
     return render(request, 'index.html', context)
-
+'''
 class IndexView(TemplateView):
     """
     This is a class-based view that renders the 'index.html' template with a context containing a name and a list of posts.
@@ -22,3 +25,24 @@ class IndexView(TemplateView):
         context['name'] = 'Amin'
         context['posts'] = Post.objects.all()
         return context
+    
+# FBV for RedirectView
+'''
+from django.shortcuts import redirect
+def redirectToMaktab(request):
+    """
+    This is a function-based view that redirects the user to the Maktabkhooneh website.
+    """
+    return redirect('https://maktabkhooneh.org/')
+'''
+
+class RedirectToMaktab(RedirectView):
+    """
+    This is a class-based view that redirects the user to the Maktabkhooneh website.
+    """
+    url = 'https://maktabkhooneh.org/'
+    
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
