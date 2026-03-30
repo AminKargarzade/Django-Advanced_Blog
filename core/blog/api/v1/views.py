e7288789274c01d3045c2b1b0f879adebdaa631b
Amin <aminkargarzadeh26@gmail.com>
2026-03-30 19:52:34 +0330

    Serializers starting point
    And showing how to response for a request!

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def postList(request):
     return Response("Ok!")
+
+@api_view()
+def postDetail(request, id):
+    post = Post.objects.get(pk=id)
+    print(post.__dict__)
+    serializer = PostSerializer(post)
+    print(serializer.data)
+    return Response(serializer.data)
