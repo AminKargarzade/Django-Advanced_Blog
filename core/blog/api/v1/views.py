ef1988011059a4e825a2cb54fc599b6964905565
Amin <aminkargarzadeh26@gmail.com>
2026-03-30 21:27:22 +0330

    What if a post does not exist?
    You can do this functionality with 2 ways!

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,15 +2,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
     return Response("Ok!")
 
+# @api_view()
+# def postDetail(request, id):
+#     try:
+#         post = Post.objects.get(pk=id)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         return Response({"detail":"post does not exist"}, status=status.HTTP_404_NOT_FOUND)
+#  You can do the same functionality with the way that I wrote down below!
+
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
-    print(post.__dict__)
+    post = get_object_or_404(Post,pk=id)
     serializer = PostSerializer(post)
-    print(serializer.data)
     return Response(serializer.data)
+    
