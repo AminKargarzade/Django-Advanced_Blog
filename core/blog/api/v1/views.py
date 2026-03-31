6f08f156d3393af94fb5cf2c214b13d0ff6634f7
Amin <aminkargarzadeh26@gmail.com>
2026-03-31 19:47:20 +0330

    Recieving Data and Saving it
    Recieving data from POST and Validation!

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,12 +5,33 @@ from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-@api_view()
-def postList(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+# @api_view(["GET","POST"])
+# def postList(request):
+#     if request.method == "GET":
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+# More compact of this code below
 
+@api_view(["GET","POST"])
+def postList(request):
+    if request.method == "GET":
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 # @api_view()
 # def postDetail(request, id):
 #     try:
