6346a0c6c0724a897574464d1d160aea5791fa35
Amin <aminkargarzadeh26@gmail.com>
2026-04-04 14:40:08 +0330

    PostDetailView in APIView form

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly,IsAdminUser
 import rest_framework.permissions
 from rest_framework.response import Response
@@ -23,7 +22,10 @@ from rest_framework.views import APIView
 #             return Response(serializer.errors)
 # More compact of this code below
 
-"""@api_view(["GET","POST"])
+"""
+from rest_framework.decorators import api_view, permission_classes
+
+@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
 def postList(request):
     if request.method == "GET":
@@ -34,7 +36,34 @@ def postList(request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)"""
+        return Response(serializer.data)
+        
+@api_view(["GET","PUT","DELETE"])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def postDetail(request, id):
+    post = get_object_or_404(Post,pk=id,status=True)
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail":"item removed successfully"}, status=status.HTTP_204_NO_CONTENT)
+        """
+        
+# @api_view()
+# def postDetail(request, id):
+#     try:
+#         post = Post.objects.get(pk=id)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         return Response({"detail":"post does not exist"}, status=status.HTTP_404_NOT_FOUND)
+#  You can do the same functionality with the way that I wrote down below!
         
 
 class PostList(APIView):
@@ -53,30 +82,29 @@ class PostList(APIView):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
-        
-# @api_view()
-# def postDetail(request, id):
-#     try:
-#         post = Post.objects.get(pk=id)
-#         serializer = PostSerializer(post)
-#         return Response(serializer.data)
-#     except Post.DoesNotExist:
-#         return Response({"detail":"post does not exist"}, status=status.HTTP_404_NOT_FOUND)
-#  You can do the same functionality with the way that I wrote down below!
+        return Response(serializer.data)    
 
-@api_view(["GET","PUT","DELETE"])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def postDetail(request, id):
-    post = get_object_or_404(Post,pk=id,status=True)
-    if request.method == "GET":
-        serializer = PostSerializer(post)
+class PostDetail(APIView):
+    """ getting detail of a post and edit plus removing it """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    
+    def get(self,request,id):
+        """ retrieving the post data """
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post)
         return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = PostSerializer(post,data=request.data)
+    
+    def put(self,request,id):
+        """ editing the post data """
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == "DELETE":
+    
+    def delete(self,request,id):
+        """ deleting a post """
+        post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({"detail":"item removed successfully"}, status=status.HTTP_204_NO_CONTENT)
