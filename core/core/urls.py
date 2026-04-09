24a08fa2e393d0637e9bf6a9f565ae93316d663c
Amin <aminkargarzadeh26@gmail.com>
2026-04-09 14:53:20 +0330

    Just a simple introduction to Documentation!

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,12 +19,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("blog/", include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("api-docs/", include_docs_urls(title='api sample'))
 ]
 
 #Serving static and media for development
