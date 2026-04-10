dec3422a47c9e8d17ad204baf6c1ca2eeb205831
Amin <aminkargarzadeh26@gmail.com>
2026-04-10 14:56:20 +0330

    Functions that depend on inner request of a serializer
    You deal with the request! That's the Reason!

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,10 +10,15 @@ class PostSerializer(serializers.ModelSerializer):
     # content = serializers.CharField(read_only=True)
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
-        fields = ['id','author','title','content','snippet','category','status','relative_url','created_date','published_date']
-        read_only_fields = ['content']
+        fields = ['id','author','title','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+        # read_only_fields = ['content']
+        
+    def get_abs_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
         
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
