cf4bd610da2d40e31a4f61afb234ff712ee38474
Amin <aminkargarzadeh26@gmail.com>
2026-04-19 23:57:33 +0330

    Implementation of TokenAuthentication!

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,14 +1,17 @@
 from django.urls import include, path
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api-v1'
 
 urlpatterns = [
     # Registration
-    path('registration/', views.RegistrationApiView.as_view(), name="registration")
+    path('registration/', views.RegistrationApiView.as_view(), name="registration"),
+    path('token/login', ObtainAuthToken.as_view(), name="token-login")
+    
     # Change Password
     # Reset password
+    
     # Login Token
-    # Login JWT
-    # 
+    # Login JWT 
 ]
