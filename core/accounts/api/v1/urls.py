175e933ff9227147acdbfa2b61f3662eec1a3f44
Amin <aminkargarzadeh26@gmail.com>
2026-04-19 12:32:21 +0330

    First Step of writing the RegistrationView
    Registration Endpoint!

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,14 @@
 from django.urls import include, path
+from . import views
+
+app_name = 'api-v1'
 
 urlpatterns = [
-    
+    # Registration
+    path('registration/', views.RegistrationApiView.as_view(), name="registration")
+    # Change Password
+    # Reset password
+    # Login Token
+    # Login JWT
+    # 
 ]
