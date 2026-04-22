c8cc1188855e0c80bb8c3274024cf47f4b0ecdd7
Amin <aminkargarzadeh26@gmail.com>
2026-04-22 12:46:55 +0330

    Destroying the Token
    Based on User request(logout)!

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -7,11 +7,12 @@ app_name = 'api-v1'
 urlpatterns = [
     # Registration
     path('registration/', views.RegistrationApiView.as_view(), name="registration"),
-    path('token/login/', views.CustomObtainAuthToken.as_view(), name="token-login")
     
     # Change Password
     # Reset password
     
     # Login Token
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name="token-logout"),
     # Login JWT 
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from .serializers import CustomAuthTokenSerializer, RegistrationSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
@@ -31,3 +33,10 @@ class CustomObtainAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+        
+class CustomDiscardAuthToken(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
