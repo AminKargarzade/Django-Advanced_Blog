4c376cd4986df35fb317732d5121980e3a59ac1f
Amin <aminkargarzadeh26@gmail.com>
2026-04-26 00:40:45 +0330

    Extending the components to manage it better!

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from . import views
+from .. import views
 # from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -7,24 +7,21 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-app_name = 'api-v1'
-
 urlpatterns = [
     # Registration
     path('registration/', views.RegistrationApiView.as_view(), name="registration"),
     
     # Change Password
     path('change-password/', views.ChangePasswordApiView.as_view(), name="change-password"),
+    
     # Reset password
     
     # Login Token
     path('token/login/', views.CustomObtainAuthToken.as_view(), name="token-login"),
     path('token/logout/', views.CustomDiscardAuthToken.as_view(), name="token-logout"),
+    
     # Login JWT
     path('jwt/create/', views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path('jwt/refresh/', TokenRefreshView.as_view(), name="jwt-refresh"),
-    path('jwt/verify/', TokenVerifyView.as_view(), name="jwt-verify"),
-    # Profile
-    path('profile/', views.ProfileApiView.as_view(), name="profile"),
-    
+    path('jwt/verify/', TokenVerifyView.as_view(), name="jwt-verify"),    
 ]
--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -1,12 +1,8 @@
-from pydoc import describe
-
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
 from django.utils.translation import ugettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 # Create your models here.
 
 class UserManager(BaseUserManager):
@@ -53,23 +49,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
-class Profile(models.Model):
-    """
-    Profile model that extends the User model with additional fields.
-    """
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=250, blank=True)
-    last_name = models.CharField(max_length=250, blank=True)
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField()
-    
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.user.email
-    
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance) 
