edc18b23fa28330284df182cd3c1029cf83e55fe
Amin <aminkargarzadeh26@gmail.com>
2026-05-31 13:39:31 +0330

    Just tidied things to not get error on CI

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-import time
 from .tasks import sendEmail
 
 
 def send_email(request):
-    sendEmail.delay() # This is the syntax how you insert the task into the view! you can give the parameters inside the delay() function like sendEmail.delay(param1, param2) 
+    sendEmail.delay()  # This is the syntax how you insert the task into the view! you can give the parameters inside the delay() function like sendEmail.delay(param1, param2)
     return HttpResponse("<h1>Email sent successfully!</h1>")
