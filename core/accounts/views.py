763f076dc7302251b4e8e46c8c5269b95147a937
Amin <aminkargarzadeh26@gmail.com>
2026-06-04 16:45:26 +0330

    Using cache_page decortaor for caching!

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 from .tasks import sendEmail
 import requests
@@ -7,8 +8,13 @@ def send_email(request):
     sendEmail.delay()  # This is the syntax how you insert the task into the view! you can give the parameters inside the delay() function like sendEmail.delay(param1, param2)
     return HttpResponse("<h1>Email sent successfully!</h1>")
 
+# def test(request):
+#     if cache.get("test_delay_api") is None:
+#         response = requests.get("https://755bf7d5-76a0-42f8-a84f-f206c8efc5cf.mock.pstmn.io/test/delay/5")
+#         cache.set("test_delay_api", response.json(), 60)  # Cache the response for 60 seconds! You can set it globally in the settings.py file as well using the CACHES variable
+#     return JsonResponse(cache.get("test_delay_api"))
+
+@cache_page(60)
 def test(request):
-    if cache.get("test_delay_api") is None:
-        response = requests.get("https://755bf7d5-76a0-42f8-a84f-f206c8efc5cf.mock.pstmn.io/test/delay/5")
-        cache.set("test_delay_api", response.json(), 60)  # Cache the response for 60 seconds! You can set it globally in the settings.py file as well using the CACHES variable
-    return JsonResponse(cache.get("test_delay_api"))
+    response = requests.get("https://755bf7d5-76a0-42f8-a84f-f206c8efc5cf.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
