0a8b07275f664ea59b600e8deeb95494d8876a7e
Amin <aminkargarzadeh26@gmail.com>
2026-06-04 15:41:12 +0330

    The Simplest way of Caching data
    It's not in API stuff yet!

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.core.cache import cache
 from .tasks import sendEmail
 import requests
 
@@ -7,5 +8,7 @@ def send_email(request):
     return HttpResponse("<h1>Email sent successfully!</h1>")
 
 def test(request):
-    response = requests.get("https://755bf7d5-76a0-42f8-a84f-f206c8efc5cf.mock.pstmn.io/test/delay/5")
-    return JsonResponse(response.json())
+    if cache.get("test_delay_api") is None:
+        response = requests.get("https://755bf7d5-76a0-42f8-a84f-f206c8efc5cf.mock.pstmn.io/test/delay/5")
+        cache.set("test_delay_api", response.json(), 60)  # Cache the response for 60 seconds! You can set it globally in the settings.py file as well using the CACHES variable
+    return JsonResponse(cache.get("test_delay_api"))
